5920378682eb6b3f45f4625eaea545fa1d1f4d8a
zrunker <1823132505@qq.com>
2022-06-22 15:59:52 +0800

    init Commit

--- ZWebView/app/src/main/java/cc/banzhi/android/utils/ClickUtil.java
+++ ZWebView/app/src/main/java/cc/banzhi/android/utils/ClickUtil.java
@@ -9,8 +9,7 @@ public class ClickUtil {
     // 判断是否为快速点击事件
     public synchronized static boolean isFastClick() {
         long time = System.currentTimeMillis();
-        if (time - lastClickTime < 500)
-            return true;
+        if (time - lastClickTime < 500) return true;
         lastClickTime = time;
         return false;
     }
